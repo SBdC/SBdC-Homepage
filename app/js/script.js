8956db4ba231cdaece2a685ebd830cdd833be547
Sara Bento de Castro <denominatu@hotmail.com>
2018-10-29 20:11:01 +0100

    reorganized index and made a lightroom

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,3 @@
-
-
-
 let today = new Date();
 let year = today.getFullYear();
 
@@ -17,36 +14,78 @@ const details = [{
     name: "green-square",
     source: "app/images/geom-pattern.jpg"
   },
-  
+
 ];
 
 
 const cards = document.querySelectorAll(".card");
+const headings = document.getElementsByTagName("h2");
+const panels = document.querySelectorAll(".panel");
 const foillard = document.getElementById('foillards')
 const images = document.querySelectorAll("img");
 const picContainer = document.querySelectorAll(".thumbnail");
 
 
-function toggleOpen (e){
+function toggleOpen(e) {
 
-  foillard.innerHTML='';
+  foillard.innerHTML = '';
   e.preventDefault();
   console.log(e.target.id);
   for (let detail in details) {
 
-  	if (e.target.id === details[detail].name) {
-  	const div = document.createElement("div");
-    div.className = "product-detail";
-    div.innerHTML = `<h3>${details[detail].name}</h3><img class="detail-image" src="${details[detail].source}">`
+    if (e.target.id === details[detail].name) {
+      const div = document.createElement("div");
+      div.className = "product-detail";
+      div.id = "product-detail";
+      const divDetail = document.getElementById('product-detail');
+      console.log(divDetail)
+      div.innerHTML = `<h3>${details[detail].name}</h3><img class="detail-image" src="${details[detail].source}">`
 
 
-  foillard.appendChild(div);
-  }
- 
+      foillard.appendChild(div);
+
+    }
+
   }
 }
 
 
 
-cards.forEach(card => card.addEventListener('click',toggleOpen, false));
 
+for (let i in headings) {
+
+  headings[i].onmouseover = function(e) {
+    for (let panel in panels) {
+
+      let hoverEl = e.target.getAttribute("data-type");
+
+      if (hoverEl === panels[panel].id) {
+        console.log("hello" + panels[panel].id);
+        let panelo = document.getElementById(panels[panel].id)
+        panelo.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0)), url(app/images/${panels[panel].id}.jpg) `
+      }
+
+    }
+  }
+
+  headings[i].onmouseout = function(e) {
+    for (let panel in panels) {
+
+      let hoverEl = e.target.getAttribute("data-type");
+      console.log(hoverEl)
+
+      if (hoverEl === panels[panel].id) {
+        console.log("goodbye" + panels[panel].id);
+        let panelo = document.getElementById(panels[panel].id)
+        panelo.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.1)), url(app/images/${panels[panel].id}.jpg) `
+      }
+
+    }
+  }
+
+}
+
+
+
+
+cards.forEach(card => card.addEventListener('click', toggleOpen, false));
