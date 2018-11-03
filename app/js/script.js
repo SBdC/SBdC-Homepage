f7d6bc58f49d45ca0979e36857cf27c7f83545a6
SBdC <denominatu@hotmail.com>
2018-11-03 16:26:33 +0100

    toggle detail

--- app/js/script.js
+++ app/js/script.js
@@ -17,7 +17,7 @@ const details = [{
   {
     type: "foillard",
     name: "green-square",
-    source: "app/images/geom-pattern.jpg",
+    sourceDetail: "geom-pattern.jpg",
     material: '100% Silk Twil',
     size: '160cm x 38cm',
     production: 'Portugal'
@@ -26,6 +26,18 @@ const details = [{
   { type: "echarpe",
     name: "bege-rect",
     source: "app/images/model_green_sq1x.jpg",
+    material: '100% Silk Twil',
+    size: '160cm x 38cm',
+    production: 'Portugal'
+  },
+   { type: "echarpe",
+    name: "navy-rect",
+    sourceDetail: "Detail-Echarpe-Narciso-e-Eco-ret-navy_1x",
+    sourceDetail2: "Detail-Echarpe-Narciso-e-Eco-ret-navy_2x",
+    sourceModel: "Model-Echarpe-Narciso-e-Eco-ret-navy_1x",
+    sourceModel2: "Model-Echarpe-Narciso-e-Eco-ret-navy_2x",
+    sourceProduct: "Echarpe-Narciso-e-Eco-ret-navy_1x",
+    sourceProduct2: "Echarpe-Narciso-e-Eco-ret-navy_2x",
     material: '100% Silk Twil',
     size: '160cm x 38cm',
     production: 'Portugal'
@@ -59,11 +71,27 @@ function toggleOpen(e) {
       const div = document.createElement("div");
       div.className = "product-detail";
       div.id = "product-detail";
-      div.innerHTML = `<img class="detail-image" src="${details[detail].source}"> 
+      div.innerHTML = ` 
+         <div class="caption">
             <p> ${details[detail].material} </p>
             <p> ${details[detail].size} </p>
             <p> Digitally printed in the Netherlands</p>
-            <p> Sewn in ${details[detail].production}</p>`;
+            <p> Sewn in ${details[detail].production}</p>
+        </div>
+   <picture class="detail-images">
+        <img class="img-detail-product" src="app/images/new/${details[detail].sourceDetail}.jpg" srcset="app/images/new/${details[detail].sourceDetail2}.jpg 2x, app/images/new/${details[detail].sourceDetail}.jpg 1x" alt="narciso eco scarf rectangular">
+  </picture>
+  <picture class="detail-images">
+       <img class="img-model-product" src="app/images/new/${details[detail].sourceModel}.jpg" srcset="app/images/new/${details[detail].sourceModel2}.jpg 2x, app/images/new/${details[detail].sourceModel}.jpg 1x" alt="narciso eco scarf rectangular">
+   </picture>
+    <picture class="detail-images">
+          <img class="img-echarpe-product" src="app/images/new/${details[detail].sourceProduct}.jpg" srcset="app/images/new/${details[detail].sourceProduct2}.jpg 2x, app/images/new/${details[detail].sourceProduct}.jpg 1x" alt="narciso eco scarf rectangular">
+   </picture>
+
+             `
+
+
+            ;
      
       if ((typeEl === details[detail].type) ){
         console.log("yo");
